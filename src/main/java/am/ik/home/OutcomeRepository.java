b4b175f4ad52da7a0628a3cea5d178a6341522d7
making <makingx@gmail.com>
2016-05-15 17:52:11 +0900

    Add findByOutcomeDate

--- src/main/java/am/ik/home/OutcomeRepository.java
+++ src/main/java/am/ik/home/OutcomeRepository.java
@@ -1,7 +1,14 @@
 package am.ik.home;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface OutcomeRepository extends CrudRepository<Outcome, Long> {
-
+    @Query("SELECT x FROM Outcome x WHERE x.outcomeDate = :outcomeDate")
+    List<Outcome> findByOutcomeDate(@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @Param("outcomeDate") LocalDate outcomeDate);
 }
